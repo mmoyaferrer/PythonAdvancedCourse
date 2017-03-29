a90e8f03b296015b25146c63e97d164270294e16
mmoyaferrer <madrilillo94@gmail.com>
2017-03-29 19:21:52 +0200

    Improved mongoDB python app
    Added some stuff

--- 0 - MongoDB/mongoDBTest.py
+++ 0 - MongoDB/mongoDBTest.py
@@ -6,17 +6,26 @@
 def get_db():
     from pymongo import MongoClient
     client = MongoClient('localhost:27017')
-    db = client.usersDB
+    db = client.userDataBase
     return db
 
-def add_user(db):
-    db.user.insert({"name" : "Manuel"})
+def add_user(db,name,surname,occupation):
+    db.userDataBase.insert({"name" : name, "skill" : surname, "occupation" : occupation})
     
-def get_user(db):
-    return db.user.find_one()
+def get_user(db,occupation):
+    return db.userDataBase.find_one({"occupation" : occupation})
 
 if __name__ == "__main__":
 
+	# Obtaining database from get_db method
     db = get_db() 
-    add_user(db)
-    print get_user(db)
+
+    # Adding five users to database
+    add_user(db,"Manuel","Ferrer","Telecom. Engineer")
+    add_user(db,"Marc","Branson","Medician")
+    add_user(db,"Adam","Branson","English Teacher")
+    add_user(db,"Daniel","Bell","RF Engineer")
+    add_user(db,"Tom","Hanks","Actor")
+
+    # Get a Telecom. engineer of database and show
+    print get_user(db,"Telecom. Engineer")
